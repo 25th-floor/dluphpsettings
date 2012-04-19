9e82663e137d6a82328c900f94232cfc970b8ecb
David Lukas <david.lukas@zfdaily.com>
2012-04-19 18:17:36 +0200

    onBootstrap

--- Module.php
+++ Module.php
@@ -3,11 +3,14 @@ namespace DluPhpSettings;
 
 use Zend\Module\Manager,
     Zend\EventManager\StaticEventManager,
+    Zend\EventManager\Event,
     Zend\Module\Consumer\AutoloaderProvider;
 
 class Module implements AutoloaderProvider
 {
     public function init(Manager $moduleManager) {
+        $events = StaticEventManager::getInstance();
+        $events->attach('bootstrap', 'bootstrap', array($this, 'onBootstrap'), 300);
     }
 
     public function getAutoloaderConfig() {
@@ -25,5 +28,20 @@ class Module implements AutoloaderProvider
 
     public function getConfig() {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Configure PHP ini settings on the bootstrap event
+     * @param \Zend\EventManager\Event $e
+     */
+    public function onBootstrap(Event $e) {
+        //$application = $e->getParam('application');
+        $config      = $e->getParam('config');
+        $phpSettings = $config['phpSettings'];
+        if($phpSettings) {
+            foreach($phpSettings as $key => $value) {
+                ini_set($key, $value);
+            }
+        }
     }
 }
